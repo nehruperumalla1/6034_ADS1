1f16817c094c6eadc2615f291a93531e6e25f79e
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-27 22:12:11 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1_assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS1_assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -4,19 +4,22 @@ import java.util.Scanner;
 class CheckBalance {
 	public void check(String str) {
 		//String str = checkstr.getParentheses();
-		String[] array = str.split("",str.length());
-		String[] stack = new String[array.length];
+		char[] array = new char[str.length()];
+		for (int i = 0; i < str.length(); i++) {
+			array[i] = str.charAt(i);
+		}
+		char[] stack = new char[array.length];
 		int size = 0;
 		for (int i = 0; i < array.length; i++) {
-			if (array[i].equals("{") || array[i].equals("(") || array[i].equals("[")) {
+			if (array[i] == '{' || array[i] == '(' || array[i] == '[') {
 				stack[size] = array[i];
 				size++;
 			} else if(size > 0) {
-				if (array[i].equals("}") && stack[size - 1].equals("{")) {
+				if (array[i] == '}' && stack[size - 1] == '{') {
 					size--;
-				}else if (array[i].equals("]") && stack[size - 1].equals("[")) {
+				}else if (array[i] == ']' && stack[size - 1] == '[') {
 					size--;
-				}else if (array[i].equals(")") && stack[size - 1].equals("(")) {
+				}else if (array[i] == ')' && stack[size - 1] == '(') {
 					size--;
 				}
 			}
@@ -30,7 +33,15 @@ class CheckBalance {
 }
 
 
+/**
+ * Class for solution.
+ */
 public class Solution {
+	/**
+	 * Main Method for Checking Balance in Parantheses.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		CheckBalance cb = new CheckBalance();
