12178068f5fa5b4876b7ef582c28ddffd5f6daae
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-27 22:15:09 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS1_assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS1_assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,35 +1,44 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Class for check balance in Parantheses.
+ */
 class CheckBalance {
-	public void check(String str) {
-		//String str = checkstr.getParentheses();
-		char[] array = new char[str.length()];
-		for (int i = 0; i < str.length(); i++) {
-			array[i] = str.charAt(i);
-		}
-		char[] stack = new char[array.length];
-		int size = 0;
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == '{' || array[i] == '(' || array[i] == '[') {
-				stack[size] = array[i];
-				size++;
-			} else if(size > 0) {
-				if (array[i] == '}' && stack[size - 1] == '{') {
-					size--;
-				}else if (array[i] == ']' && stack[size - 1] == '[') {
-					size--;
-				}else if (array[i] == ')' && stack[size - 1] == '(') {
-					size--;
-				}
-			}
-		}
-		if (size == 0) {
-			System.out.println("YES");
-		} else {
-			System.out.println("NO");
-		}
-	}
+
+    /**
+     * This method will check the balance in Parantheses.
+     *
+     * @param      str   The string
+     */
+    public void check(String str) {
+        //String str = checkstr.getParentheses();
+        char[] array = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            array[i] = str.charAt(i);
+        }
+        char[] stack = new char[array.length];
+        int size = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == '{' || array[i] == '(' || array[i] == '[') {
+                stack[size] = array[i];
+                size++;
+            } else if(size > 0) {
+                if (array[i] == '}' && stack[size - 1] == '{') {
+                    size--;
+                } else if (array[i] == ']' && stack[size - 1] == '[') {
+                    size--;
+                } else if (array[i] == ')' && stack[size - 1] == '(') {
+                    size--;
+                }
+            }
+        }
+        if (size == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
 }
 
 
@@ -37,18 +46,24 @@ class CheckBalance {
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * Main Method for Checking Balance in Parantheses.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		CheckBalance cb = new CheckBalance();
-		int n = scan.nextInt();
-		while (scan.hasNext()) {
-			String str = scan.next();
-			cb.check(str);
-		}
-	}
+    /**
+     * Constructs the Object.
+     */
+    private Solution() {
+        //Not Using this Constructor.
+    }
+    /**
+     * Main Method for Checking Balance in Parantheses.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        CheckBalance cb = new CheckBalance();
+        int n = scan.nextInt();
+        while (scan.hasNext()) {
+            String str = scan.next();
+            cb.check(str);
+        }
+    }
 }
