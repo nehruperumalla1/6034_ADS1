171df5bc8f5d94f412102ab96ea7da02cee0e18a
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-09-29 18:46:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS1_assignments/m5/5.2 Josephus Problem/Solution.java
+++ ADS1_assignments/m5/5.2 Josephus Problem/Solution.java
@@ -1,92 +1,21 @@
 import java.util.Scanner;
-import java.util.Arrays;
-class Node {
-    int data;
-    Node next;
-    Node(int value) {
-        data = value;
-        next = null;
-    }
-    // Node head;
-    public Node getAddress() {
-        return next;
-    }
-    public void setAddress(Node address) {
-        next = address;
-    }
-
-    public void setdata(int data1) {
-        data = data1;
-    }
-}
-
-class Operations {
-    Node head;
-    Node tail;
-    int size;
-    public void insertAtTail(int data) {
-        Node end = new Node(data);
-        size++;
-        if (head == null) {
-            head = end;
-            tail = end;
-            return;
-        }
-        tail.next = end;
-        tail = end;
-        return;
-    }
-
-    public void popAtHead() {
-        if (size == 0) {
-            System.out.println("Deck is empty");
-            return;
-        }
-        Node temp = head;
-        head = head.getAddress();
-        temp = null;
-        size--;
-    }
-    public String printList() {
-        Node temp = head;
-        String str = "[";
-        if (size > 0) {
-            while (temp.next != null) {
-                str += temp.data + ", ";
-                temp = temp.getAddress();
-            }
-            str += temp.data;
-        }
-        str += "]";
-        return str;
-    }
-    public int size() {
-        return size;
-    }
-
- 	public int[] josephus(int m, int size) {
-        int[] arr = new int[size];
-        Node temp = head;
-        int counter = 1;
-        int i = 0;
-        while (temp.next != null) {
-            while (counter != m) {
-                insertAtTail(temp.data);
-                temp = temp.next;
-                popAtHead();
-                counter++;
-            }
-        arr[i++] = temp.data;
-        temp = temp.next;
-        counter = 1;
-        popAtHead();
-        }
-        arr[i++] = temp.data;
-        return arr;
-   }
-}
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//Not USing This.
+	}
+
+	/**
+	 * Main Method for Josephus Problem.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
 		scan.nextLine();
