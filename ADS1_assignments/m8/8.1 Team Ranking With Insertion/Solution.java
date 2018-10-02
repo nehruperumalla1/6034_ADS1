b3d9f0c7bfb8c16a615e031c837b28a007495330
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-10-02 20:52:05 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- ADS1_assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS1_assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -35,46 +35,46 @@ class TeamInfo {
         draws = draw;
     }
 
-	/**
-	 * Method to check which value of Team is higher is lower.
-	 *
-	 * @param      that  The that
-	 *
-	 * @return     { Returns the boolean Value true if there is need to swap
-	 *               else false}
-	 */
-	public boolean less(TeamInfo that) {
-		if (this.wins < that.wins) {
-			return true;
-		}
-		if (this.wins > that.wins) {
-			return false;
-		} else {
-			if (this.losses < that.losses) {
-				return false;
-			}
-			if (this.losses > that.losses) {
-				return true;
-			} else {
-				if (this.draws < that.draws) {
-					return true;
-				}
-				if (this.draws > that.draws) {
-					return false;
-				}
-			}
-		}
-		return false;
-	}
+    /**
+     * Method to check which value of Team is higher is lower.
+     *
+     * @param      that  The that
+     *
+     * @return     { Returns the boolean Value true if there is need to swap
+     *               else false}
+     */
+    public boolean less(final TeamInfo that) {
+        if (this.wins < that.wins) {
+            return true;
+        }
+        if (this.wins > that.wins) {
+            return false;
+        } else {
+            if (this.losses < that.losses) {
+                return false;
+            }
+            if (this.losses > that.losses) {
+                return true;
+            } else {
+                if (this.draws < that.draws) {
+                    return true;
+                }
+                if (this.draws > that.draws) {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * Gets the Team Name.
-	 *
-	 * @return     The name.
-	 */
-	public String getName() {
-		return this.name;
-	}
+    /**
+     * Gets the Team Name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.name;
+    }
 }
 
 
@@ -83,98 +83,99 @@ class TeamInfo {
  */
 class SelectionSort {
 
-	/**
-	 * Sorting of TeamInfo Objects based on Wins, losses, draws
-	 * using selection sort.
-	 *
-	 * @param      teams  The teams
-	 *
-	 * @return     { Returns the sorted array of objects }
-	 */
-	public TeamInfo[] selectsort(TeamInfo[] teams) {
-		int min = 0;
-		for (int i = 0; i < teams.length; i++) {
-			min = i;
-			for (int j = i + 1; j < teams.length; j++) {
-				if (teams[min].less(teams[j])) {
-					min = j;
-				}
-			}
-			teams = exchange(teams, min, i);
-		}
-		return teams;
-	} 
+    /**
+     * Sorting of TeamInfo Objects based on Wins, losses, draws
+     * using selection sort.
+     *
+     * @param      teams  The teams
+     *
+     * @return     { Returns the sorted array of objects }
+     */
+    public TeamInfo[] selectsort(final TeamInfo[] teams) {
+        int min = 0;
+        TeamInfo[] sortedteams = teams;
+        for (int i = 0; i < sortedteams.length; i++) {
+            min = i;
+            for (int j = i + 1; j < sortedteams.length; j++) {
+                if (sortedteams[min].less(sortedteams[j])) {
+                    min = j;
+                }
+            }
+            sortedteams = exchange(sortedteams, min, i);
+        }
+        return teams;
+    }
 
-	/**
-	 * This method is used to swap the objects in an array.
-	 *
-	 * @param      teams  The teams
-	 * @param      min    The minimum
-	 * @param      index  The index
-	 *
-	 * @return     { Returns the array after swapping the objects }
-	 */
-	public TeamInfo[] exchange(TeamInfo[] teams, int min, int index) {
-		TeamInfo temp = teams[min];
-		teams[min] = teams[index];
-		teams[index] = temp;
-		return teams;
-	}
+    /**
+     * This method is used to swap the objects in an array.
+     *
+     * @param      teams  The teams
+     * @param      min    The minimum
+     * @param      index  The index
+     *
+     * @return     { Returns the array after swapping the objects }
+     */
+    public TeamInfo[] exchange(final TeamInfo[] teams, final int min, final int index) {
+        TeamInfo temp = teams[min];
+        teams[min] = teams[index];
+        teams[index] = temp;
+        return teams;
+    }
 }
 
 /**
  * Class for lead board.
  */
 class LeadBoard {
-	/**
-	 * Object of Selection Sort class.
-	 */
-	private SelectionSort sort = new SelectionSort();
-	/**
-	 * Creates the object array of Class TeamInfo.
-	 */
-	private TeamInfo[] teams;
-	/**
-	 * Size of the Object Array.
-	 */
-	private int size;
+    /**
+     * Object of Selection Sort class.
+     */
+    private SelectionSort sort = new SelectionSort();
+    /**
+     * Creates the object array of Class TeamInfo.
+     */
+    private TeamInfo[] teams;
+    /**
+     * Size of the Object Array.
+     */
+    private int size;
 
-	/**
-	 * Constructs the object.
-	 */
-	LeadBoard() {
-		teams = new TeamInfo[1];
-		size = 0;
-	}
+    /**
+     * Constructs the object.
+     */
+    LeadBoard() {
+        teams = new TeamInfo[1];
+        size = 0;
+    }
 
-	/**
-	 * Resize Method for Resizing object Array.
-	 */
-	public void resize() {
-		teams = Arrays.copyOf(teams, teams.length + 1);
-	}
+    /**
+     * Resize Method for Resizing object Array.
+     */
+    public void resize() {
+        teams = Arrays.copyOf(teams, teams.length + 1);
+    }
 
-	/**
-	 * Adds a team to Object Array.
-	 *
-	 * @param      team  The team
-	 */
-	public void addTeam(TeamInfo team) {
-		if (size == teams.length) {
-			resize();
-		}
-		teams[size++] = team;
-	}
-	/**
-	 * To Print the Team Names after sorting.
-	 */
-	public void print() {
-		TeamInfo[] sortedteams = sort.selectsort(teams);
-		for (int i = 0; i < size - 1; i++) {
-			System.out.print(sortedteams[i].getName() + ",");
-		}
-		System.out.println(sortedteams[size - 1].getName());
-	}
+    /**
+     * Adds a team to Object Array.
+     *
+     * @param      team  The team
+     */
+    public void addTeam(final TeamInfo team) {
+        if (size == teams.length) {
+            resize();
+        }
+        teams[size++] = team;
+    }
+    /**
+     * To Print the Team Names after sorting.
+     */
+    public void print() {
+        TeamInfo[] sortedteams = sort.selectsort(teams);
+        for (int i = 0; i < size - 1; i++) {
+            System.out.print(sortedteams[i].getName() + ",");
+        }
+        System.out.println(sortedteams[size - 1].getName());
+    }
 }
 
 
@@ -182,21 +183,27 @@ class LeadBoard {
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Main Method for Sorting the Teams using Selection Sort.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		LeadBoard cricketobj = new LeadBoard();
-		Scanner scan = new Scanner(System.in);
-		String[] info = null;
-		while (scan.hasNext()) {
-			info = scan.nextLine().split(",");
-			cricketobj.addTeam(new TeamInfo(info[0], Integer.parseInt(info[1]),
-				Integer.parseInt(info[2]), Integer.parseInt(info[3])));
-		}
-		cricketobj.print();
-	}
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Not Using this Constructor.
+    }
+    /**
+     * Main Method for Sorting the Teams using Selection Sort.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        LeadBoard cricketobj = new LeadBoard();
+        Scanner scan = new Scanner(System.in);
+        String[] info = null;
+        while (scan.hasNext()) {
+            info = scan.nextLine().split(",");
+            cricketobj.addTeam(new TeamInfo(info[0], Integer.parseInt(info[1]),
+                Integer.parseInt(info[2]), Integer.parseInt(info[2 + 2 + 1])));
+        }
+        cricketobj.print();
+    }
 }
