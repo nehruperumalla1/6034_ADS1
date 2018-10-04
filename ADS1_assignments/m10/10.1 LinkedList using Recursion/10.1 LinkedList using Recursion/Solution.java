604c657449be52396b41d2cccd8963db88f4bc51
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-10-04 18:08:32 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1_assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
+++ ADS1_assignments/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 /**
  * Class for node.
  */
@@ -59,102 +58,149 @@ class Node {
     }
 }
 
+/**
+ * Class for linked list record.
+ */
 class LinkedListRec {
-	Node head;
-	Node curr;
-	Node before;
-	Node after;
-	int size;
-	int counter;
-	LinkedListRec() {
-		head = null;
-		before = null;
-		counter = 1;
-		size = 0;
-	}
+    /**
+     * Head Node of the Linked List points to First Element in the List.
+     */
+    private Node head;
+    /**
+     * Curr Node of the Linked List points to elements in List.
+     */
+    private Node curr;
+    /**
+     * Before Node points to left Node to Curr Node in the List.
+     */
+    private Node before;
+    /**
+     * After Node points to right Node to Curr Node in the ist
+     */
+    private Node after;
+    /**
+     * Size of the List.
+     */
+    private int size;
+    /**
+     * Counter to used insert element at Particular Location.
+     */
+    private int counter;
+    /**
+     * Constructs the object.
+     */
+    LinkedListRec() {
+        head = null;
+        before = null;
+        counter = 1;
+        size = 0;
+    }
 
-	public void insertAt(int pos, int value) throws Exception {
-		Node position = new Node(value);
-		if (size < pos || pos < 0) {
-			throw new Exception("Can't insert at this position.");
-		}
-		if (head == null) {
-			head = position;
-			curr = head;
-			size++;
-			return;
-		}else if (pos == 0) {
-			position.setNext(head);
-			head = position;
-			curr = head;
-			size++;
-			return;
-		}else if (pos == counter) {
-			position.setNext(curr.getNext());
-			curr.setNext(position);
-			curr = head;
-			counter = 1;
-			size++;
-			return;
-		}
-		curr = curr.getNext();
-		counter++;
-		insertAt(pos, value);
-	}
+    /**
+     * Inserting value at Particaular Location Using Recursion.
+     *
+     * @param      pos        The position
+     * @param      value      The value
+     *
+     * @throws     Exception  { If pos is invalid }
+     */
+    public void insertAt(int pos, int value) throws Exception {
+        Node position = new Node(value);
+        if (size < pos || pos < 0) {
+            throw new Exception("Can't insert at this position.");
+        }
+        if (head == null) {
+            head = position;
+            curr = head;
+            size++;
+            return;
+        } else if (pos == 0) {
+            position.setNext(head);
+            head = position;
+            curr = head;
+            size++;
+            return;
+        } else if (pos == counter) {
+            position.setNext(curr.getNext());
+            curr.setNext(position);
+            curr = head;
+            counter = 1;
+            size++;
+            return;
+        }
+        curr = curr.getNext();
+        counter++;
+        insertAt(pos, value);
+    }
 
-	public void reverse() throws Exception {
-		if (size == 0) {
-			throw new Exception("No elements to reverse.");
-		}
-		after = curr.getNext();
-		curr.setNext(before);
-		before = curr;
-		if (after == null) {
-			head = curr;
-			before = null;
-			return ;
-		}
-		curr = after;
-		reverse();
-	}
+    /**
+     * Reverse the Linked List using recursion.
+     *
+     * @throws     Exception  { Reversing if Size is 0 }
+     */
+    public void reverse() throws Exception {
+        if (size == 0) {
+            throw new Exception("No elements to reverse.");
+        }
+        after = curr.getNext();
+        curr.setNext(before);
+        before = curr;
+        if (after == null) {
+            head = curr;
+            before = null;
+            return ;
+        }
+        curr = after;
+        reverse();
+    }
 
-	public void printList() {
-		while (curr.getNext() != null) {
-			System.out.print(curr.getData() + ", ");
-			curr = curr.getNext();
-		}
-		System.out.println(curr.getData());
-		curr = head;
-	}
+    /**
+     * It is used to print the Linked List.
+     */
+    public void printList() {
+        while (curr.getNext() != null) {
+            System.out.print(curr.getData() + ", ");
+            curr = curr.getNext();
+        }
+        System.out.println(curr.getData());
+        curr = head;
+    }
 }
 
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		LinkedListRec list = new LinkedListRec();
-		Scanner scan = new Scanner(System.in);
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(" ");
-			switch (tokens[0]) {
-				case "insertAt":
-				try {
-					list.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
-					list.printList();
-				} catch(Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-				case "reverse":
-				try {
-					list.reverse();
-					list.printList();
-				} catch(Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-				default:
-				break;
-			}
-		}
-
-	}
+    /**
+     * Main Method for implementing LinkedList Using Recursion.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        LinkedListRec list = new LinkedListRec();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            switch (tokens[0]) {
+                case "insertAt":
+                try {
+                    list.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    list.printList();
+                } catch(Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+                case "reverse":
+                try {
+                    list.reverse();
+                    list.printList();
+                } catch(Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+                default:
+                break;
+            }
+        }
+    }
 }
