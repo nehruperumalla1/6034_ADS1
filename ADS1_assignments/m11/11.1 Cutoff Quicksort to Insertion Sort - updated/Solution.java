c171cafe85706c3a9148cd31631f5b2bb563c07c
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-10-06 17:40:34 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS1_assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
+++ ADS1_assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
@@ -1,114 +1,208 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+/**
+ * Class for insertion sort.
+ */
 class InsertionSort {
-	InsertionSort() {
-		// System.out.println("In Insertion Sort");
-	}
+    /**
+     * Constructs the object.
+     */
+    InsertionSort() {
+        // System.out.println("In Insertion Sort");
+    }
 
-	public Comparable[] insertionSort(Comparable[] a, int lo, int hi) {
-		return sort(a, lo, hi);
-	}
+    /**
+     * Public Method to sort Array of elements.
+     *
+     * @param      a     { parameter_description }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Comparable[] insertionSort(final Comparable[] a,
+        final int lo, final int hi) {
+        return sort(a, lo, hi);
+    }
 
-	private Comparable[] sort(Comparable[] a, int lo, int hi) {
-		int j = 0;
-		// System.out.println(hi + " Hi");
-		for (int i = lo + 1; i <= hi; i++) {
-			j = i - 1;
-			Comparable temp = a[i];
-			while (j >= 0 && temp.compareTo(a[j]) < 0) {
-				a[j + 1] = a[j];
-				j--;
-			}
-			a[j + 1] = temp;
-		}
-		return a;
-	}
+    /**
+     * private Method to Sort Array of elements using Insertion Sort.
+     * The Complexity of Insertion Sort is O(N^2)
+     * It has two loops for comparing and swapping the elements
+     * For this the worst case complexity is O(N^2)
+     *
+     * @param      a     { Array elements to Sort }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     *
+     * @return     { Sorted Array }
+     */
+    private Comparable[] sort(final Comparable[] a,
+        final  int lo, final int hi) {
+        int j = 0;
+        // System.out.println(hi + " Hi");
+        for (int i = lo + 1; i <= hi; i++) {
+            j = i - 1;
+            Comparable temp = a[i];
+            while (j >= 0 && temp.compareTo(a[j]) < 0) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = temp;
+        }
+        return a;
+    }
 }
 
+/**
+ * Class for quick sort.
+ */
 class QuickSort {
+    /**
+     * Cutoff Specified by User.
+     */
+    private int cutoff;
+    /**
+     * Object for insertion Sort Class.
+     */
+    private InsertionSort insertion;
+    /**
+     * Constructs the object.
+     *
+     * @param      cut   The cut
+     */
+    QuickSort(final int cut) {
+        cutoff = cut;
+        insertion = new InsertionSort();
+    }
 
-	int cutoff;
-	InsertionSort insertion;
-	QuickSort(int cut) {
-		cutoff = cut;
-		insertion = new InsertionSort();
-	}
+    /**
+     * Method to call Private Quick Sort Method.
+     *
+     * @param      a       { Array to be Sorted }
+     */
+    public void qSort(final Comparable[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        qSort(a, lo, hi);
+    }
 
-	public void qSort(Comparable[] a, int cutoff) {
-		int lo = 0;
-		int hi = a.length - 1;
-		int cut = cutoff;
-		qSort(a, lo, hi); 
-	}
+    /**
+     * Method to Sort the elements using Quick Sort.
+     * The complexity is O(N^2)
+     * This is not Stable based upon pivot element selection.
+     * It iterates two loops to find the pivot element and sort.
+     *
+     * @param      a     { Arrat=y to be Sorted }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     */
+    private void qSort(final Comparable[] a, final int lo, final int hi) {
+        // System.out.println("In Quick Sort   " + cutoff);
+        if (hi - lo + 1 <= cutoff) {
+            insertion.insertionSort(a, lo, hi);
+            System.out.println("insertionSort called");
+            return;
+        }
+        int j = partition(a, lo, hi);
+        qSort(a, lo, j - 1);
+        qSort(a, j + 1, hi);
+    }
 
-	private void qSort(Comparable[] a, int lo, int hi) {
-		// System.out.println("In Quick Sort   " + cutoff);
-		if (hi - lo + 1 <= cutoff) {
-			insertion.insertionSort(a, lo, hi);
-			System.out.println("insertionSort called");
-			return;
-		}
-		int j = partition(a, lo, hi);
-		qSort(a, lo, j - 1);
-		qSort(a, j + 1, hi);
-	}
+    /**
+     * Partition the Array using Pivot Element.
+     *
+     * @param      a     { Array to be Sorted }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     *
+     * @return     { Index of the Paartitioned element in Array }
+     */
+    private int partition(final Comparable[] a, final int lo, final int hi) {
+        int i = lo + 1;
+        int k = lo;
+        int j = hi;
+        while (true) {
+            while (less(a[i], a[k])) {
+                i++;
+                if (i == hi) {
+                    break;
+                }
+            }
+            while (less(a[k], a[j])) {
+                j--;
+                if (j == lo) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            exchange(a, i, j);
+            i++;
+        }
+        exchange(a, j, k);
+        System.out.println(Arrays.toString(a));
+        return j;
+    }
 
-	private int partition(Comparable[] a, int lo, int hi) {
-		int i = lo + 1;
-		int k = lo;
-		int j = hi;
-		while (true) {
-			while (less(a[i], a[k])) {
-				i++;
-				if (i == hi) {
-					break;
-				}
-			}
-			while (less(a[k], a[j])) {
-				j--;
-				if (j == lo) {
-					break;
-				}
-			}
-			if (i >= j) {
-				break;
-			}
-			exchange(a, i, j);
-			i++;
-		}
-		exchange(a, j, k);
-		System.out.println(Arrays.toString(a));
-		return j;
-	}
+    /**
+     * Less method to Compare the Less one.
+     *
+     * @param      a     { Object Comparable 1 }
+     * @param      b     { Object Comparable 2 }
+     *
+     * @return     { True or False based upon less value than 0. }
+     */
+    private boolean less(final Comparable a, final Comparable b) {
+        return a.compareTo(b) < 0;
+    }
 
-	private boolean less(Comparable a, Comparable b) {
-		return a.compareTo(b) < 0;
-	}
-
-	private void exchange(Comparable[] a, int i, int k) {
-		Comparable temp = a[k];
-		a[k] = a[i];
-		a[i] = temp;
-	}
+    /**
+     * Exchange method swap the elements in the Array.
+     *
+     * @param      a     { Array of comparable type. }
+     * @param      i     { index of element to be Swapped }
+     * @param      k     { index of second element to be swapped }
+     */
+    private void exchange(final Comparable[] a, final int i, final int k) {
+        Comparable temp = a[k];
+        a[k] = a[i];
+        a[i] = temp;
+    }
 }
 
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
-		while (num > 0) {
-			int cutoff = scan.nextInt();
-			scan.nextLine();
-			String[] tokens = scan.nextLine().split(" ");
-			if (tokens[0].length() == 0) {
-				System.out.println("[]");
-			} else {
-				QuickSort quick = new QuickSort(cutoff);
-				quick.qSort(tokens, cutoff);
-				System.out.println(Arrays.toString(tokens));
-			}
-			num--;
-		}
-	}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * Main Method to Sort the Elements using Quick Sort.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        while (num > 0) {
+            int cutoff = scan.nextInt();
+            scan.nextLine();
+            String[] tokens = scan.nextLine().split(" ");
+            if (tokens[0].length() == 0) {
+                System.out.println("[]");
+            } else {
+                QuickSort quick = new QuickSort(cutoff);
+                quick.qSort(tokens);
+                System.out.println(Arrays.toString(tokens));
+            }
+            num--;
+        }
+    }
 }
